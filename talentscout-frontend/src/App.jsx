cb008d89dacec6edcb838e15f9de8f9297904701
dinukah99 <dinukahamarasinghe@gmail.com>
2024-01-11 14:07:03 +0530

    build configuration

--- talentscout-frontend/src/App.jsx
+++ talentscout-frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold text-red-500">This is TalentScout Testing</h1>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <h1 className="text-3xl font-bold text-red-500">This is TalentScout Testing</h1>
+            </div>
+        </>
+    )
 }
 
 export default App
