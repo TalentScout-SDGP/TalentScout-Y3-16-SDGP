9d24fdf5529ec221dca569daa12d0437031ae185
dinukah99 <dinukahamarasinghe@gmail.com>
2024-01-18 14:00:03 +0530

    Added submit and social sign up buttons

--- talentscout-frontend/src/components/SignUp.jsx
+++ talentscout-frontend/src/components/SignUp.jsx
@@ -4,9 +4,9 @@ function SignUp() {
     return (
         <div className="font-poppins">
             <div className="container py-16 grid grid-cols-2">
-                <div className="ps-16">
+                <div className="px-16">
                     <div className="text-primary-ts_blue text-2xl font-bold">Create your account</div>
-                    <form className="w-full max-w-lg py-8">
+                    <form className="w-full py-8">
                         <div className="flex flex-wrap -mx-3 my-2">
                             <div className="w-full px-3 md:mb-0">
                                 <label className="text-primary-ts_blue text-lg font-semibold">Username</label>
@@ -49,13 +49,36 @@ function SignUp() {
                                 </select>
                             </div>
                         </div>
-                        <div className="flex lg:block justify-center lg:justify-end mt-8">
+                        <div className="flex flex-wrap mt-8 mb-6">
                             <button
-                                className="bg-primary-footer_button text-black py-2 px-6 font-semibold rounded-lg transition-transform duration-3000 transform hover:scale-105">
-                                Send
+                                className="w-full bg-primary-ts_blue text-white py-3 px-6 font-semibold rounded-lg">
+                                Create account
                             </button>
                         </div>
                     </form>
+                    <div className="flex items-center justify-start">
+                        <div className="border-t border-black h-0 w-1/4"></div>
+                        <p className="mx-4">Or Sign up with</p>
+                        <div className="border-t border-black h-0 w-1/4"></div>
+                    </div>
+                    <div className="flex flex-wrap mt-8 mb-2">
+                        <button
+                            className="block w-full bg-white text-primary-ts_blue font-semibold border border-black rounded-lg py-3 px-4 mt-1 mb-3 shadow-signup leading-tight">
+                            Sign up with Google
+                        </button>
+                    </div>
+                    <div className="flex flex-wrap mb-6">
+                        <button
+                            className="block w-full bg-white text-primary-ts_blue font-semibold border border-black rounded-lg py-3 px-4 mt-1 mb-3 shadow-signup leading-tight">
+                            Sign up with Facebook
+                        </button>
+                    </div>
+                    <div>
+                        Already have an account?&nbsp;
+                        <a className="underline cursor-pointer font-semibold text-primary-ts_blue" href="#">
+                            Log in
+                        </a>
+                    </div>
                 </div>
                 <div>
                     <div className="text-primary-ts_blue text-2xl font-bold">Create your account</div>
