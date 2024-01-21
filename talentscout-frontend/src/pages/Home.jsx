57777b3c0c289e337431fadf471830556208c941
dinukah99 <dinukahamarasinghe@gmail.com>
2024-01-21 12:09:45 +0530

    added text for hero section

--- talentscout-frontend/src/pages/Home.jsx
+++ talentscout-frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import HomeManagePlayers from "../components/HomeManagePlayers.jsx";
+import HomeHero from "../components/HomeHero.jsx";
 
 const Home = () => {
     return (
         <div className="">
+            <HomeHero/>
             <HomeManagePlayers/>
         </div>
     )
