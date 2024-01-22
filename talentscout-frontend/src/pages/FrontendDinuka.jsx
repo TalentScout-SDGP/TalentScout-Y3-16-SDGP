11bff0b825389cfcf39923d9adf6a0c451253cfe
dinukah99 <dinukahamarasinghe@gmail.com>
2024-01-22 16:06:34 +0530

    player-info section initial structure - compare players page

--- talentscout-frontend/src/pages/FrontendDinuka.jsx
+++ talentscout-frontend/src/pages/FrontendDinuka.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import HomeManagePlayers from "../components/HomeManagePlayers.jsx";
 import HomeHero from "../components/HomeHero.jsx";
 import HomeComparePlayers from "../components/HomeComparePlayers.jsx";
+import ComparePlayerInfo from "../components/ComparePlayerInfo.jsx";
 
 const FrontendDinuka = () => {
     return (
         <div>
-            <HomeHero/>
-            <HomeComparePlayers/>
-            <HomeManagePlayers/>
+            <ComparePlayerInfo/>
+            {/*<HomeHero/>*/}
+            {/*<HomeComparePlayers/>*/}
+            {/*<HomeManagePlayers/>*/}
         </div>
     );
 }
