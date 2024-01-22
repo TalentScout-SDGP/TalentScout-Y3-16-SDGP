6a2b61df2d8a6bbf9c7c0743cecd357d80aff5b9
dinukah99 <dinukahamarasinghe@gmail.com>
2024-01-22 15:11:58 +0530

    homepage compares players section - responsive & finalised

--- talentscout-frontend/src/components/HomeComparePlayers.jsx
+++ talentscout-frontend/src/components/HomeComparePlayers.jsx
@@ -1,36 +1,40 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import {faSquarePlus, faBook, faPenToSquare, faTrash} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 function HomeManagePlayers() {
-    const iconSize = "2x";
 
     return (
         <div className="font-poppins">
             <div className="md:container px-8">
                 <div
-                    className="bg-primary-ts_purple flex flex-col gap-y-6 justify-center items-center rounded-lg p-16">
+                    className="bg-primary-ts_purple flex flex-col gap-y-6 justify-center items-center rounded-lg p-8 lg:p-16">
                     <div className="flex flex-col justify-center gap-y-4">
                         <p className="text-black text-base lg:text-2xl font-bold text-center leading-relaxed">
-                            Duis vel arcu porta
+                            Discover Cricket Excellence
                         </p>
-                        <p className="text-black text-base lg:text-lg font-bold text-center leading-relaxed w-4/5 mx-auto">
-                            Duis vel arcu porta sapien fringilla congue. Praesent mollis turpis quam, vel hendrerit
-                            lectus faucibus blandit. Nunc sollicitudin odio eu sem mattis congue. Etiam eu leo vehicula.
+                        <p className="hidden lg:block text-black text-sm lg:text-lg font-semibold text-center lg:w-4/5 mx-auto">
+                            Elevate your cricket experience! Our platform empowers
+                            you to delve into the depths of
+                            player prowess, comparing vital stats and insights effortlessly. Uncover the secrets behind
+                            each performance and make informed choices. Witness the game-changing details that matter.
+                        </p>
+                        <p className="block lg:hidden text-black text-sm lg:text-lg font-semibold text-center lg:w-4/5 mx-auto">
+                            Elevate your cricket experience! Our platform empowers
+                            you to delve into the depths of
+                            player prowess, comparing vital stats and insights effortlessly.
                         </p>
                     </div>
-                    <div className="grid grid-cols-3 justify-center items-center gap-x-8">
+                    <div className="grid grid-cols-3 justify-center items-center gap-x-4 lg:gap-x-8">
                         <div className="text-center">
-                            <img className="w-3/5 mx-auto" src={import.meta.env.BASE_URL + 'Player-dummy-image.png'}
+                            <img className="lg:w-3/5 mx-auto" src={import.meta.env.BASE_URL + 'Player-dummy-image.png'}
                                  alt="player-dummy-image"></img>
                         </div>
                         <div className="text-center">
-                            <img className="w-2/5 mx-auto" src={import.meta.env.BASE_URL + 'versus-image.png'}
+                            <img className="lg:w-2/5 mx-auto" src={import.meta.env.BASE_URL + 'versus-image.png'}
                                  alt="versus-image"></img>
                         </div>
                         <div className="text-center">
-                            <img className="w-3/5 mx-auto" src={import.meta.env.BASE_URL + 'Player-dummy-image.png'}
+                            <img className="lg:w-3/5 mx-auto" src={import.meta.env.BASE_URL + 'Player-dummy-image.png'}
                                  alt="player-dummy-image"></img>
                         </div>
                     </div>
