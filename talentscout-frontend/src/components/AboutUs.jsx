2a692a36915a7008ca4e30d1a9f9700814e6cf49
Linuka Rivi Rihan <linukarivirihan@gmail.com>
2024-01-23 13:48:50 +0530

    About us page call option added

--- talentscout-frontend/src/components/AboutUs.jsx
+++ talentscout-frontend/src/components/AboutUs.jsx
@@ -42,6 +42,16 @@ function AboutUs() {
                             </a>
 
                             <div className="border-l border-gray-500 h-40 lg:block hidden"></div>
+
+                            <a href="tel:+94112345678">
+                                <div
+                                    className="flex flex-col 2xl:w-48 text-center items-center p-6 md:p-4 hover:shadow-outer hover:rounded-3xl hover:scale-105 transition-transform duration-2000 transform">
+                                    <img className="w-24 h-24 md:w-20 md:h-20 p-4 md:p-0"
+                                         src={import.meta.env.BASE_URL + 'call-option.png'}
+                                         alt="Person"/>
+                                    <p className="font-bold md:pt-4 lg:w-24 xl:w-">Contact us through a phone call</p>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </div>
