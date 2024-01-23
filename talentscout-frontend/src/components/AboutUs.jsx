3373f7385672c4ccb0be803b483731c79a485800
Linuka Rivi Rihan <linukarivirihan@gmail.com>
2024-01-23 14:05:34 +0530

    About us page hero section done

--- talentscout-frontend/src/components/AboutUs.jsx
+++ talentscout-frontend/src/components/AboutUs.jsx
@@ -15,45 +15,6 @@ function AboutUs() {
                         </div>
                         <img className="size-80 mx-auto 2xl:mx-2" src={import.meta.env.BASE_URL + 'default-image.png'} alt=""/>
                     </div>
-
-                    <div className="mx-auto p-8 mt-8 mb-10">
-                        <div
-                            className="2xl:w-11/12 2xl:space-x-28 lg:flex lg:justify-center items-center lg:space-x-24 sm:text-xs sm:grid sm:grid-cols-1">
-                            <a href="">
-                                <div
-                                    className="flex flex-col 2xl:w-48 text-center items-center p-6 md:p-4 hover:shadow-outer hover:rounded-3xl hover:scale-105 transition-transform duration-2000 transform">
-                                    <img className="w-24 h-24 md:w-20 md:h-20 p-4 md:p-0"
-                                         src={import.meta.env.BASE_URL + 'person-option.png'}
-                                         alt="Person"/>
-                                    <p className="font-bold md:pt-4 lg:w-24">Meet our dedicated sales team</p>
-                                </div>
-                            </a>
-
-                            <div className="border-l border-gray-500 h-40 lg:block hidden"></div>
-
-                            <a href="mailto:">
-                                <div
-                                    className="flex flex-col 2xl:w-48 text-center items-center p-6 md:p-4 hover:shadow-outer hover:rounded-3xl hover:scale-105 transition-transform duration-2000 transform">
-                                    <img className="w-24 h-24 md:w-20 md:h-20 p-4 md:p-0"
-                                         src={import.meta.env.BASE_URL + 'email-option.png'}
-                                         alt="Person"/>
-                                    <p className="font-bold md:pt-4 lg:w-24">Email us for prompt communication</p>
-                                </div>
-                            </a>
-
-                            <div className="border-l border-gray-500 h-40 lg:block hidden"></div>
-
-                            <a href="tel:+94112345678">
-                                <div
-                                    className="flex flex-col 2xl:w-48 text-center items-center p-6 md:p-4 hover:shadow-outer hover:rounded-3xl hover:scale-105 transition-transform duration-2000 transform">
-                                    <img className="w-24 h-24 md:w-20 md:h-20 p-4 md:p-0"
-                                         src={import.meta.env.BASE_URL + 'call-option.png'}
-                                         alt="Person"/>
-                                    <p className="font-bold md:pt-4 lg:w-24 xl:w-">Contact us through a phone call</p>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
