d307956047cfe7babd8eab40b2153e65505a41ca
Linuka Rivi Rihan <linukarivirihan@gmail.com>
2024-01-24 16:20:02 +0530

    Manage Players page hero section

---
+++ talentscout-frontend/src/components/ManagePlayers_LP.jsx
@@ -0,0 +1,18 @@
+function ManagePlayersLp() {
+    return (
+        <div className="font-poppins">
+            <div className="md:container mt-20 mb-20">
+                <div className="bg-primary-ts_blue w-12/12 h-40 justify-center">
+                    <p className="text-white text-base text-left lg:text-lg">
+                        The ultimate hub for coaches, team managers and unmatched teams hungry for excellence. With a sleek interface
+                        designed for efficiency, users can easily use search, add, update and delete options. This tool allows you to
+                        sculpt and fine-tune your team with precision. Experience the power of strategic player management and ensure
+                        your roster reflects your vision for victory.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ManagePlayersLp;
