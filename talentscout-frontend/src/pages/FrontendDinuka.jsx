b9f903c269a5b38388b972f77f0876f873f4407a
dinukah99 <dinukahamarasinghe@gmail.com>
2024-01-24 20:13:30 +0530

    removing flowbite package

--- talentscout-frontend/src/pages/FrontendDinuka.jsx
+++ talentscout-frontend/src/pages/FrontendDinuka.jsx
@@ -3,10 +3,12 @@ import HomeHero from "../components/HomeHero.jsx";
 import HomeComparePlayers from "../components/HomeComparePlayers.jsx";
 import ComparePlayerInfo from "../components/ComparePlayerInfo.jsx";
 import ComparePlayerStats from "../components/ComparePlayerStats.jsx";
+import ComparePlayersSearch from "../components/ComparePlayersSearch.jsx";
 
 const FrontendDinuka = () => {
     return (
         <div>
+            <ComparePlayersSearch/>
             <ComparePlayerInfo/>
             <ComparePlayerStats/>
             {/*<HomeHero/>*/}
