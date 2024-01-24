c62dd2bfaf4d9c256f0a1a178a25a0278b005e3e
SulanChandila <109819795+SulanChandila@users.noreply.github.com>
2024-01-24 21:07:54 +0530

    Minor update fixes in Compare Players page

--- talentscout-frontend/src/components/ComparePlayersSearch.jsx
+++ talentscout-frontend/src/components/ComparePlayersSearch.jsx
@@ -4,23 +4,12 @@ function ComparePlayersSearch() {
         <div className="font-poppins">
             <div className="md:container px-8 my-2">
                 <div
-                    className="grid grid-cols-1 md:grid-cols-3 bg-primary-ts_blue p-0 mx-2 my-2 overflow-hidden">
-                    <div
-                        className="hidden md:flex w-full xl:w-44 2xl:w-44 2xl:mx-52 xl:mx-20 lg:w-36 lg:mx-18 md:w-24 items-center text-center col-span-4">
-                        <img src={import.meta.env.BASE_URL + 'compare-players-image.png'} alt="search-icon"
-                             className="min-[600px]:w-40 w-20 mx-10 2xl:px-8 xl:px-2 py-6"></img>
-                        <img src={import.meta.env.BASE_URL + 'compare-players-image.png'} alt="search-icon"
-                             className="min-[600px]:w-40 w-20 mx-10 2xl:px-8 xl:px-2 py-6"></img>
-                        <img src={import.meta.env.BASE_URL + 'compare-players-image.png'} alt="search-icon"
-                             className="min-[600px]:w-40 w-20 mx-10 2xl:px-8 xl:px-2 py-6"></img>
-                        <img src={import.meta.env.BASE_URL + 'compare-players-image.png'} alt="search-icon"
-                             className="min-[600px]:w-40 w-20 mx-10 2xl:px-8 xl:px-2 py-6"></img>
-                    </div>
+                    className="grid grid-cols-1 md:grid-cols-3 shadow-outer rounded-3xl bg-primary-ts_blue my-8  px-10 sm:px-10 py-8">
                     <div
                         className="bg-primary-ts_blue grid grid-cols-1 mt-2 mb-6 sm:mt-4 my-12 px-12 sm:px-10">
                         <div className="flex flex-col items-center justify-center">
                             <input type="text"
-                                   className="w-44 lg:w-64 md:w-48 bg-primary-ts_purple sm:w-80 py-2 rounded-lg my-3 text-left ps-6"
+                                   className="w-48 2xl:w-80 lg:w-64 md:w-48 bg-primary-ts_purple sm:w-80 py-2 rounded-2xl my-3 text-left ps-6"
                                    placeholder="Player Name">
                             </input>
                             <button className="text-sm bg-primary-yellow text-black font-semibold rounded-button px-6 py-1 lg:py-2 shadow-lg border-primary-ts_blue border-2
@@ -46,7 +35,7 @@ function ComparePlayersSearch() {
                         className="bg-primary-ts_blue grid grid-cols-1 mb-6 mt-2 my-12 sm:mt-4 px-10 sm:px-10">
                         <div className="flex flex-col items-center justify-center">
                             <input type="text"
-                                   className="w-44 lg:w-64 md:w-48 bg-primary-ts_purple sm:w-80 py-2 rounded-lg mb-3 text-left ps-6"
+                                   className="w-48 2xl:w-80 lg:w-64 md:w-48 bg-primary-ts_purple sm:w-80 py-2 rounded-2xl mb-3 text-left ps-6"
                                    placeholder="Player Name">
                             </input>
                             <button className="text-sm bg-primary-yellow text-black font-semibold rounded-button px-6 py-1 lg:py-2 shadow-lg border-primary-ts_blue border-2
