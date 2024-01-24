1f28166c1a6685985258db297c933effc5fab6b1
Linuka Rivi Rihan <linukarivirihan@gmail.com>
2024-01-24 15:56:30 +0530

    Minor fixes

--- talentscout-frontend/src/components/AboutUs.jsx
+++ talentscout-frontend/src/components/AboutUs.jsx
@@ -6,7 +6,7 @@ function AboutUs() {
                     <div className="lg:flex sm:grid sm:grid-cols-1 2xl:w-11/12 md:items-center md:justify-center mx-auto mt-16 p-8 bg-primary-ts_purple rounded-2xl shadow-outer">
                         <div className="lg:w-2/3 lg:flex lg:flex-col md:grid md:grid-cols-1 text-center md:text-center lg:text-left">
                             <div className="text-3xl font-bold pt-8">ABOUT US</div>
-                            <p className="lg:max-w-xl sm:text-xs md:text-base font-bold pt-12 pb-10">Welcome to
+                            <p className="lg:max-w-xl sm:text-xs md:text-base text-justify pt-12 pb-10">Welcome to
                                 TalentScout, an advanced platform tailored exclusively
                                 for cricket enthusiasts seeking a comprehensive and refined experience. At TalentScout,
                                 our commitment is to redefine and elevate the
@@ -14,7 +14,7 @@ function AboutUs() {
                                 this remarkable journey where the sophistication
                                 of the game meets intuitive tools for a refined cricket experience.</p>
                         </div>
-                        <img className="size-80 mx-auto 2xl:mx-2" src={import.meta.env.BASE_URL + 'about_us.png'} alt=""/>
+                        <img className="size-80 mx-auto 2xl:mx-2 rounded-3xl" src={import.meta.env.BASE_URL + 'about_us.png'} alt=""/>
                     </div>
 
                     <div className="mx-auto p-8 mt-8 mb-10">
