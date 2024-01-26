d273ba31b5e274da293079cacb41369de2e961a1
Agrani-V <agrani.20212188@iit.ac.lk>
2024-01-26 23:26:55 +0530

    Fixing some differences in codes to as it is first

--- talentscout-frontend/src/pages/ManagePlayers.jsx
+++ talentscout-frontend/src/pages/ManagePlayers.jsx
@@ -1,10 +1,8 @@
 import CRUDManagePlayers from "../components/CRUDManagePlayers.jsx";
-import ManagePlayers_LP from "../components/ManagePlayers_LP.jsx";
 
 const ManagePlayers = () => {
     return (
         <div>
-            <ManagePlayers_LP/>
             <CRUDManagePlayers/>
         </div>
     )
