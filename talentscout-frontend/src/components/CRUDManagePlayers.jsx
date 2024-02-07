814899c444728f824c464b5ae0c5be0ed3452b06
Agrani-V <agrani.20212188@iit.ac.lk>
2024-02-07 18:10:20 +0530

    Changed Manage Players text size

--- talentscout-frontend/src/components/CRUDManagePlayers.jsx
+++ talentscout-frontend/src/components/CRUDManagePlayers.jsx
@@ -51,7 +51,7 @@ const CRUDManagePlayers = () => {
     return (
         <div className="font-poppins py-0">
             <div className="md:container mx-auto px-4 lg:px-0">
-                <div className="flex justify-between items-center bg-primary-ts_blue text-white text-sm p-4 rounded-t-3xl ">
+                <div className="flex justify-between items-center bg-primary-ts_blue text-white  p-4  rounded-t-3xl ">
                     <strong>Manage Players</strong>
                     <div className="flex items-center space-x-2">
                         <button
