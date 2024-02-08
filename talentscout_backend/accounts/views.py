5bb7eb4bd1921d6a8c346754623cf394417baddb
dinukah99 <dinukahamarasinghe@gmail.com>
2024-02-08 16:12:39 +0530

    login serializer, verify user email and login views

--- talentscout_backend/accounts/views.py
+++ talentscout_backend/accounts/views.py
@@ -1,8 +1,9 @@
 from rest_framework.generics import GenericAPIView
-from .serializers import UserRegisterSerializer
+from .serializers import UserRegisterSerializer, LoginSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from .utils import sendOtp
+from .models import User, OneTimePassword
 
 
 class RegisterUserView(GenericAPIView):
@@ -24,3 +25,33 @@ class RegisterUserView(GenericAPIView):
                            f'verify your email to complete the registration.'
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class VerifyUserEmail(GenericAPIView):
+    def post(self, request):
+        otp_code = request.data.get('otp')
+        try:
+            user_code_obj = OneTimePassword.objects.get(code=otp_code)
+            user = user_code_obj.user
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+                return Response({
+                    'message': 'Email verified successfully'
+                }, status=status.HTTP_200_OK)
+            return Response({
+                'message': 'Email already verified'
+            }, status=status.HTTP_204_NO_CONTENT)
+        except OneTimePassword.DoesNotExist:
+            return Response({
+                'message': 'Invalid OTP'
+            }, status=status.HTTP_404_NOT_FOUND)
+
+
+class LoginUserView(GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
