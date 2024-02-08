0b6bac652a437113155a247b6680d51ff91b0ae7
dinukah99 <dinukahamarasinghe@gmail.com>
2024-02-08 15:58:58 +0530

    creating user model

--- talentscout_backend/accounts/models.py
+++ talentscout_backend/accounts/models.py
@@ -1,4 +1,40 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from .managers import UserManager
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your models here.
-# (Class Based)
+AUTH_PROVIDERS = {'email': 'email', 'google': 'google', 'github': 'github'}
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True, verbose_name=_('email Address'))
+    first_name = models.CharField(max_length=100, verbose_name=_('First Name'))
+    last_name = models.CharField(max_length=100, verbose_name=_('Last Name'))
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    auth_provider = models.CharField(max_length=50, default=AUTH_PROVIDERS.get('email'))
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
