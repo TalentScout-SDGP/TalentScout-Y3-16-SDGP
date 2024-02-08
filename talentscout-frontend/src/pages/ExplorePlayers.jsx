28c4861ed5f97f01936e1657c15a85f5203a09ca
M.D.C.D.Mahapatuna <109390299+ChamathDMahapatuna@users.noreply.github.com>
2024-02-08 17:12:27 +0530

    Fix some responsive issues

--- talentscout-frontend/src/pages/ExplorePlayers.jsx
+++ talentscout-frontend/src/pages/ExplorePlayers.jsx
@@ -1,7 +1,10 @@
+import ExplorePlayerRanking from "../components/Explore_Player_Ranking.jsx";
+
 const ExplorePlayers = () => {
     return (
         <div>
             <h1>Explore Players Page</h1>
+            <ExplorePlayerRanking/>
         </div>
     )
 }
