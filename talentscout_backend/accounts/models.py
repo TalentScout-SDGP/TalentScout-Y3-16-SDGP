87ca6eef1effe9f48effcbdef5c91a0a63e7fdb9
dinukah99 <dinukahamarasinghe@gmail.com>
2024-02-08 16:01:04 +0530

    creating model for one time password

--- talentscout_backend/accounts/models.py
+++ talentscout_backend/accounts/models.py
@@ -38,3 +38,11 @@ class User(AbstractBaseUser, PermissionsMixin):
             'refresh': str(refresh),
             'access': str(refresh.access_token)
         }
+
+
+class OneTimePassword(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6, unique=True)
+
+    def __str__(self):
+        return f"{self.user.email}-passcode"
