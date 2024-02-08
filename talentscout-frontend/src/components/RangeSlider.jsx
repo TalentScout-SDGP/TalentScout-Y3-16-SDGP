040730fba8a22e51083a9f23c0ae27811975e419
SulanChandila <109819795+SulanChandila@users.noreply.github.com>
2024-02-08 12:23:02 +0530

    Fixing padding issues in explore players page

--- talentscout-frontend/src/components/RangeSlider.jsx
+++ talentscout-frontend/src/components/RangeSlider.jsx
@@ -11,7 +11,7 @@ function MultiRange() {
 
     return (
         <div>
-            <div className="range py-8 lg:py-0">
+            <div className="range pt-8 lg:py-0">
                 <MultiRangeSlider
                     min={12}
                     max={100}
