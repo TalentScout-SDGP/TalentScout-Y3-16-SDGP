493756c7e3c7d21a38ad104371c324faa79b1edc
M.D.C.D.Mahapatuna <109390299+ChamathDMahapatuna@users.noreply.github.com>
2024-02-11 18:25:35 +0530

    SDGP-351 Added ODI Bowler content

--- talentscout-frontend/src/components/AddNewPlayersStats.jsx
+++ talentscout-frontend/src/components/AddNewPlayersStats.jsx
@@ -66,7 +66,7 @@ function AddNewPlayersStats() {
                 <div className="bg-primary-ts_purple my-12 lg:w-full rounded-lg px-16 py-24">
                     {/* Main Tabs */}
                     <div
-                        className="section-header flex justify-center gap-x-2 sm:gap-x-4 bg-primary-Tab_color w-fit mx-auto py-1 px-2 rounded-3xl lg:text-base sm:text-sm text-xs">
+                        className="section-header flex justify-center gap-x-2 sm:gap-x-4 bg-primary-Tab_color w-fit mx-auto mb-5 py-1 px-2 rounded-3xl lg:text-base sm:text-sm text-xs">
                         <button onClick={() => handleMainTabChange('ODI')}
                                 className={activeMainTab === 'ODI' ? 'active text-sm sm:text-lg font-semibold bg-primary-ts_blue text-white py-1 px-4 sm:px-8 rounded-2xl' : 'font-semibold text-sm sm:text-lg py-1 px-4 sm:px-8'}>
                             ODI
@@ -308,7 +308,7 @@ function AddNewPlayersStats() {
 
                                         {/*}*/}
                                         <button type="submit"
-                                                className="bg-primary-yellow lg:w-44 text-primary-ts_blue p-2 font-medium  border-2 border-primary-ts_blue rounded-3xl mt-10 shadow-md">
+                                                className="bg-primary-yellow lg:w-44 sm:w-44 text-primary-ts_blue p-2 font-medium border-2 border-primary-ts_blue rounded-3xl mt-10 shadow-md mx-auto sm:mx-0 sm:justify-center">
                                             Submit
                                         </button>
                                     </form>
@@ -316,7 +316,195 @@ function AddNewPlayersStats() {
 
 
                             }
-                            {activeSubTab === 'Bowler' && <p>ODI Bowler Content</p>}
+
+                            {activeSubTab === 'Bowler' &&
+                                <div className="tab-content my-8 bg-primary-ts_purple rounded-lg lg:text-base text-sm">
+                                    <form onSubmit={handleSubmit} className="w-full py-4 lg:py-8">
+                                        <div className="lg:grid lg:grid-cols-2 lg:pt-8 lg:py-4 font-semibold lg:px-8">
+                                            <div className="mb-4 lg:pr-5 lg:mr-4">
+                                                <div className="mb-4 sm:flex-wrap lg:flex items-center justify-between">
+                                                    <label htmlFor="matches"
+                                                           className="block text-md font-bold sm:mr-5 mb-5 text-ts_blue  mt-1">
+                                                        Matches:</label>
+                                                    <input
+                                                        type="text"
+                                                        id="matches"
+                                                        value={matches}
+                                                        onChange={(e) => setMatches(e.target.value)}
+                                                        required
+                                                        className="w-full lg:w-3/5 sm:w-full md:w-full p-1 text-2 border-2 border-primary-ts_blue rounded-3xl mb-5 shadow-md"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div className="mb-4 lg:pr-5 lg:mr-4  ">
+                                                <div className="mb-4 sm:flex-wrap lg:flex items-center justify-between">
+                                                    <label htmlFor="innings"
+                                                           className="block text-md font-bold sm:mr-5 mb-5 text-ts_blue  mt-4">
+                                                        Innings:
+                                                    </label>
+                                                    <input
+                                                        type="text"
+                                                        id="innings"
+                                                        value={innings}
+                                                        onChange={(e) => setInnings(e.target.value)}
+                                                        required
+                                                        className="w-full lg:w-3/5 sm:w-full md:w-full  p-1 border-2 border-primary-ts_blue rounded-3xl mb-5 shadow-md"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div className="mb-4 lg:pr-5 lg:mr-4">
+                                                <div className="mb-4 sm:flex-wrap lg:flex items-center justify-between">
+                                                    <label htmlFor="Wickets"
+                                                           className="block text-md font-bold sm:mr-5 mb-5 text-ts_blue  mt-4">
+                                                        Wickets :
+                                                    </label>
+                                                    <input
+                                                        type="text"
+                                                        id="Wickets"
+                                                        value={Wickets}
+                                                        onChange={(e) => setWickets(e.target.value)}
+                                                        required
+                                                        className="w-full lg:w-3/5 sm:w-full md:w-full p-1 text-2 border-2 border-primary-ts_blue rounded-3xl mb-5 shadow-md"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div className="mb-4 lg:pr-5 lg:mr-4">
+                                                <div className="mb-4 sm:flex-wrap lg:flex items-center justify-between">
+                                                    <label htmlFor="Overs"
+                                                           className="block text-md font-bold sm:mr-5 mb-5 text-ts_blue  mt-4">
+                                                        Overs :
+                                                    </label>
+                                                    <input
+                                                        type="text"
+                                                        id="Overs"
+                                                        value={Overs}
+                                                        onChange={(e) => setOvers(e.target.value)}
+                                                        required
+                                                        className="w-full lg:w-3/5 sm:w-full md:w-full p-1 text-2 border-2 border-primary-ts_blue rounded-3xl mb-5 shadow-md"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div className="mb-4 lg:pr-5 lg:mr-4">
+                                                <div className="mb-4 sm:flex-wrap lg:flex items-center justify-between">
+                                                    <label htmlFor="runs"
+                                                           className="block text-md font-bold sm:mr-5 mb-5 text-ts_blue  mt-4">
+                                                        Runs :
+                                                    </label>
+                                                    <input
+                                                        type="text"
+                                                        id="runs"
+                                                        value={runs}
+                                                        onChange={(e) => setRuns(e.target.value)}
+                                                        required
+                                                        className="w-full lg:w-3/5 sm:w-full md:w-full p-1 text-2 border-2 border-primary-ts_blue rounded-3xl mb-5 shadow-md"
+                                                    /></div>
+                                            </div>
+                                            <div className="mb-4 lg:pr-5 lg:mr-4">
+                                                <div className="mb-4 sm:flex-wrap lg:flex items-center justify-between">
+                                                    <label htmlFor="BBI"
+                                                           className="block text-md font-bold sm:mr-5 mb-5 text-ts_blue  mt-4">
+                                                        BBI:
+                                                    </label>
+                                                    <input
+                                                        type="text"
+                                                        id="BBI"
+                                                        value={BBI}
+                                                        onChange={(e) => setBBI(e.target.value)}
+                                                        required
+                                                        className="w-full lg:w-3/5 sm:w-full md:w-full p-1 text-2 border-2 border-primary-ts_blue rounded-3xl mb-5 shadow-md"
+                                                    />
+                                                </div>
+                                            </div>
+                                            {/* Right Column */}
+                                            <div className="mb-4 lg:pr-5 lg:mr-4">
+                                                <div className="mb-4 sm:flex-wrap lg:flex items-center justify-between">
+                                                    <label htmlFor="Econ"
+                                                           className="block text-md font-bold sm:mr-5 mb-5 text-ts_blue  mt-4">
+                                                        Econ:
+                                                    </label>
+                                                    <input
+                                                        type="text"
+                                                        id="Econ"
+                                                        value={Econ}
+                                                        onChange={(e) => setEcon(e.target.value)}
+                                                        required
+                                                        className="w-full lg:w-3/5 sm:w-full md:w-full p-1 text-2 border-2 border-primary-ts_blue rounded-3xl mb-5 shadow-md"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div className="mb-4 lg:pr-5 lg:mr-4">
+                                                <div className="mb-4 sm:flex-wrap lg:flex items-center justify-between">
+                                                    <label htmlFor="average"
+                                                           className="block text-md font-bold sm:mr-5 mb-5 text-ts_blue  mt-4">
+                                                        Average:
+                                                    </label>
+                                                    <input
+                                                        type="text"
+                                                        id="average"
+                                                        value={average}
+                                                        onChange={(e) => setAverage(e.target.value)}
+                                                        required
+                                                        className="w-full lg:w-3/5 sm:w-full md:w-full p-1 text-2 border-2 border-primary-ts_blue rounded-3xl mb-5 shadow-md"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div className="mb-4 lg:pr-5 lg:mr-4">
+                                                <div className="mb-4 sm:flex-wrap lg:flex items-center justify-between">
+                                                    <label htmlFor="strikeRate"
+                                                           className="block text-md font-bold sm:mr-5 mb-5 text-ts_blue  mt-4">
+                                                        StrikeRate:
+                                                    </label>
+                                                    <input
+                                                        type="text"
+                                                        id="strikeRate"
+                                                        value={strikeRate}
+                                                        onChange={(e) => setStrikeRate(e.target.value)}
+                                                        required
+                                                        className="w-full lg:w-3/5 sm:w-full md:w-full p-1 text-2 border-2 border-primary-ts_blue rounded-3xl mb-5 shadow-md"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div className="mb-4 lg:pr-5 lg:mr-4">
+                                                <div className="mb-4 sm:flex-wrap lg:flex items-center justify-between">
+                                                    <label htmlFor="FourWickets"
+                                                           className="block text-md font-bold sm:mr-5 mb-5 text-ts_blue  mt-4">
+                                                        4Ws :
+                                                    </label>
+                                                    <input
+                                                        type="text"
+                                                        id="FourWickets"
+                                                        value={FourWickets}
+                                                        onChange={(e) => setFourWickets(e.target.value)}
+                                                        required
+                                                        className="w-full lg:w-3/5 sm:w-full md:w-full p-1 text-2 border-2 border-primary-ts_blue rounded-3xl mb-5 shadow-md"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div className="mb-4 lg:pr-5 lg:mr-4">
+                                                <div className="mb-4 sm:flex-wrap lg:flex items-center justify-between">
+                                                    <label htmlFor="FiveWickets"
+                                                           className="block text-md font-bold sm:mr-5 mb-5 text-ts_blue  mt-4">
+                                                        5Ws :
+                                                    </label>
+                                                    <input
+                                                        type="text"
+                                                        id="FiveWickets"
+                                                        value={FiveWickets}
+                                                        onChange={(e) => setFiveWickets(e.target.value)}
+                                                        required
+                                                        className="w-full lg:w-3/5 sm:w-full md:w-full p-1 text-2 border-2 border-primary-ts_blue rounded-3xl mb-5 shadow-md"
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {/*}*/}
+                                        <button type="submit"
+                                                className="bg-primary-yellow lg:w-44 sm:w-44 text-primary-ts_blue p-2 font-medium border-2 border-primary-ts_blue rounded-3xl mt-10 shadow-md mx-auto sm:mx-0 sm:justify-center">
+                                            Submit
+                                        </button>
+                                    </form>
+                                </div>
+                            }
                             {activeSubTab === 'WicketKeeper' && <p>ODI Wicket Keeper Content</p>}
                         </div>
                     )}
