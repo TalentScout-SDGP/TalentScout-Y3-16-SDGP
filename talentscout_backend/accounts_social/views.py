856fe2b8fce65372a9690246246006dfebccdfa6
dinukah99 <dinukahamarasinghe@gmail.com>
2024-03-01 13:14:56 +0530

    github auth backend

--- talentscout_backend/accounts_social/views.py
+++ talentscout_backend/accounts_social/views.py
@@ -1,5 +1,5 @@
 from rest_framework.generics import GenericAPIView
-from .serializers import GoogleSignInSerializer
+from .serializers import GoogleSignInSerializer, GithubSignInSerializer
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -12,3 +12,14 @@ class GoogleSignInView(GenericAPIView):
         serializer.is_valid(raise_exception=True)
         data = (serializer.validated_data['access_token'])
         return Response(data, status=status.HTTP_200_OK)
+
+
+class GithubSignInView(GenericAPIView):
+    serializer_class = GithubSignInSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            data = (serializer.validated_data['code'])
+            return Response(data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
