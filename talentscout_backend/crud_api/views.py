46d61dbb757d0ac682ffbd4048829a8352fe1e16
dinukah99 <dinukahamarasinghe@gmail.com>
2024-03-01 21:34:37 +0530

    refactored create player function to auto assign ids

--- talentscout_backend/crud_api/views.py
+++ talentscout_backend/crud_api/views.py
@@ -54,46 +54,51 @@ def getPlayerById(request, player_id):
 @api_view(['POST'])
 def createPlayer(request):
     if request.method == 'POST':
+        latest_player_id = Player.objects.latest('player_id')
+        next_player_id = latest_player_id.player_id + 1 if latest_player_id else 1
+        request.data['player_id'] = next_player_id
         player_serializer = PlayerSerializer(data=request.data)
         if player_serializer.is_valid():
             player_instance = player_serializer.save()
 
-            # get relevant stats data from request.data
             batting_data_list = request.data.get('batting_data', [])
             bowling_data_list = request.data.get('bowling_data', [])
             wicketkeeping_data_list = request.data.get('wicketkeeping_data', [])
 
             for batting_data in batting_data_list:
-                # Adding player reference to related data
                 batting_data['player'] = player_instance.player_id
-                # Serializing and saving related instances
+                batting_data['batting_id'] = setUniqueID(player_instance.player_id, batting_data['format'])
+                batting_data['player_id'] = player_instance.player_id
                 batting_serializer = PlayerBattingSerializer(data=batting_data)
                 if batting_serializer.is_valid():
                     batting_serializer.save()
                 else:
-                    print('Error Batting')
                     print(batting_serializer.errors)
 
             for bowling_data in bowling_data_list:
-                # Adding player reference to related data
                 bowling_data['player'] = player_instance.player_id
-                # Serializing and saving related instances
+                bowling_data['bowling_id'] = setUniqueID(player_instance.player_id, bowling_data['format'])
+                bowling_data['player_id'] = player_instance.player_id
                 bowling_serializer = PlayerBowlingSerializer(data=bowling_data)
                 if bowling_serializer.is_valid():
                     bowling_serializer.save()
                 else:
-                    print('Error Bowling')
                     print(bowling_serializer.errors)
 
             for wicketkeeping_data in wicketkeeping_data_list:
-                # Adding player reference to related data
                 wicketkeeping_data['player'] = player_instance.player_id
-                # Serializing and saving related instances
+                wicketkeeping_data['wicketkeeping_id'] = setUniqueID(player_instance.player_id,
+                                                                     wicketkeeping_data['format'])
+                wicketkeeping_data['player_id'] = player_instance.player_id
                 wicketkeeping_serializer = PlayerWicketKeepingSerializer(data=wicketkeeping_data)
                 if wicketkeeping_serializer.is_valid():
                     wicketkeeping_serializer.save()
                 else:
-                    print('Error Wicketkeeping')
                     print(wicketkeeping_serializer.errors)
             return Response(player_serializer.data, status=status.HTTP_201_CREATED)
         return Response(player_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def setUniqueID(player_id, playing_format):
+    formats = {'Test': 100, 'ODI': 200, 'T20': 300}
+    return int(str(player_id) + str(formats[playing_format]))
