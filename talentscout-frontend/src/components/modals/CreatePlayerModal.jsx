7bd05aec64cf1de4fdd3279aba120088f7d8cbfd
dinukah99 <dinukahamarasinghe@gmail.com>
2024-03-09 17:19:34 +0530

    custom modal styling

--- talentscout-frontend/src/components/modals/CreatePlayerModal.jsx
+++ talentscout-frontend/src/components/modals/CreatePlayerModal.jsx
@@ -20,8 +20,10 @@ export function CreatePlayerModal() {
             <Dialog open={open} handler={handleOpen}>
                 <Card className="mx-auto lg:px-12">
                     <CardBody className="flex flex-col">
+                        <img className="w-2/5 mx-auto select-none" src={import.meta.env.BASE_URL + 'green-check.png'}
+                             alt="green-check"></img>
                         <p className="text-sm sm:text-md md:text-base lg:text-xl text-black text-center font-semibold border-b-2 border-gray-400 pb-2">
-                            Player Created Successfully
+                            Player Created Successfully!
                         </p>
                         <Link to='/player_profiles'
                               className="w-full bg-primary-ts_blue text-center text-white font-semibold text-sm sm:text-md md:text-base py-2 rounded-lg mt-6">
