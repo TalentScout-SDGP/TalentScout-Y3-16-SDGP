bbbb39c2dd0c6cb17a13847603546775cb33c6c7
DulhanPerera <vicksura.20210165@iit.ac.lk>
2024-03-10 14:36:21 +0530

    Adding email.js to the footer

--- talentscout-frontend/src/components/shared/Footer.jsx
+++ talentscout-frontend/src/components/shared/Footer.jsx
@@ -21,3 +21,41 @@ export const ContactUs = () => {
         },
       );
   };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <div className="flex flex-wrap -mx-3 my-2">
+        <div className="w-full md:w-1/2 px-3 md:mb-0">
+          <input
+            className="appearance-none block w-full bg-primary-footer_gray text-white placeholder-white border rounded-2xl py-3 px-4 mb-3 leading-tight focus:outline-none text-sm md:text-md lg:text-base"
+            id="footer-first-name" type="text" placeholder="First Name" name="user_name" />
+        </div>
+        <div className="w-full md:w-1/2 px-3">
+          <input
+            className="appearance-none block w-full bg-primary-footer_gray text-white placeholder-white border rounded-2xl py-3 px-4 mb-3 leading-tight focus:outline-none text-sm md:text-md lg:text-base"
+            id="footer-last-name" type="text" placeholder="Last Name" />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-2">
+        <div className="w-full px-3">
+          <input
+            className="appearance-none block w-full bg-primary-footer_gray text-white placeholder-white border rounded-2xl py-3 px-4 mb-3 leading-tight focus:outline-none text-sm md:text-md lg:text-base"
+            id="footer-email" type="email" placeholder="Enter Your Email" name="user_email" />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-2">
+        <div className="w-full px-3">
+          <textarea id="message" rows="6"
+            className="appearance-none block w-full bg-primary-footer_gray text-white placeholder-white border rounded-2xl py-3 px-4 mb-3 leading-tight focus:outline-none resize-none text-sm md:text-md lg:text-base"
+            placeholder="Message" name="message"></textarea>
+        </div>
+      </div>
+      <div className="flex lg:block justify-center lg:justify-end">
+        <button
+          className="lg:float-end bg-primary-footer_button text-black py-2 px-6 font-semibold rounded-2xl transition-transform duration-3000 transform hover:scale-105 text-sm md:text-md lg:text-base">Send
+        </button>
+      </div>
+    </form>
+  );
+};
+
