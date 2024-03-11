a73d71b8f32088ef03a92cb4aa94c3b791d0e1e5
M.D.C.D.Mahapatuna <109390299+ChamathDMahapatuna@users.noreply.github.com>
2024-03-11 20:10:15 +0530

    ML backend | Adding pickle model

--- talentscout_backend/playeridentification/views.py
+++ talentscout_backend/playeridentification/views.py
@@ -5,6 +5,17 @@ from .serializers import FormDataSerializer
 from crud_api.models import Player, PlayerBatting, PlayerBowling, PlayerWicketKeeping
 from django.db.models import Q
 from crud_api.serializers import PlayerBattingSerializer, PlayerBowlingSerializer, PlayerWicketKeepingSerializer
+import pickle
+import csv
+import pandas as pd
+
+# Global variables initialization
+format_input = "odi"
+playing_role = "bowling"
+wicketkeeping_stats_order = ['Matches', 'Innings', 'Highest Score', 'Balls Faced', '100s', '6s', 'Catches', 'Runs',
+                             'Not Outs', 'Average', 'Strike Rate', '50s', '4s', 'Stumps']
+bowling_stats_order = ['Matches', 'Wickets', 'Innings', 'Overs', 'Runs', 'BBI', 'Avg', 'Econ', 'SR', '4Ws', '5Ws']
+batting_stats_order = ['Matches', 'Runs', 'Innings', 'NO', 'HS', 'Avg', 'BF', 'SR', '100s', '50s', '4s', '6s']
 
 
 @api_view(['POST'])
@@ -68,16 +79,20 @@ def rankPlayers(request):
                 player_stats = {}
                 for stat in stats:
                     for key, value in stat.items():
-                        if key == 'format':
+                        if key != 'batting_id' and key != 'format':
                             player_stats[key] = value
-                            stats_values.append([value])  # Create a new list for 'format' key
-                        elif key != 'batting_id':
-                            player_stats[key] = value
-                            stats_values[-1].append(value)  # Append values to the last list in stats_values
-
+                            stats_values.append(value)
                 stats_list.append(player_stats)
 
             print(stats_values)
+
+            grouped_stats_values = [stats_values[i:i + 13] for i in range(0, len(stats_values), 13)]
+            print(grouped_stats_values)
+
+            # model_path = 'talentscout_backend/playeridentification/Pickle_Models/trained_Batting_TEST_model.pkl'  # Adjust the path accordingly
+            # with open(model_path, 'rb') as file:
+            #     loaded_model = pickle.load(model_file)
+            # new_player_stats = pd.DataFrame([stats_order], columns=numeric_columns)
 
             return Response(stats_list, status=status.HTTP_200_OK)
 
