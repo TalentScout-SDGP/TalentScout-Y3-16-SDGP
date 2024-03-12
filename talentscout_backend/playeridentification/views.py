7eb187f4f828ab7d34ebd714838d6ca4fb720863
M.D.C.D.Mahapatuna <109390299+ChamathDMahapatuna@users.noreply.github.com>
2024-03-12 21:19:05 +0530

    ML backend | using filters to take the database data

--- talentscout_backend/playeridentification/views.py
+++ talentscout_backend/playeridentification/views.py
@@ -4,58 +4,34 @@ from rest_framework import status
 from .serializers import FormDataSerializer
 from crud_api.models import Player, PlayerBatting, PlayerBowling, PlayerWicketKeeping
 from django.db.models import Q
-from crud_api.serializers import PlayerBattingSerializer, PlayerBowlingSerializer, PlayerWicketKeepingSerializer
-import pickle
-import csv
-import pandas as pd
-import os
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-
-# Global variables initialization
-format_input = "odi"
-playing_role = "bowling"
-wicketkeeping_stats_order = ['Matches', 'Innings', 'Highest Score', 'Balls Faced', '100s', '6s', 'Catches', 'Runs',
-                             'Not Outs', 'Average', 'Strike Rate', '50s', '4s', 'Stumps']
-bowling_stats_order = ['Matches', 'Wickets', 'Innings', 'Overs', 'Runs', 'BBI', 'Avg', 'Econ', 'SR', '4Ws', '5Ws']
-batting_stats_order = ['Matches', 'Runs', 'Innings', 'NO', 'HS', 'Avg', 'BF', 'SR', '100s', '50s', '4s', '6s']
+from crud_api.serializers import PlayerSerializer, PlayerBattingSerializer, PlayerBowlingSerializer, \
+    PlayerWicketKeepingSerializer
 
 
 @api_view(['POST'])
 def rankPlayers(request):
-    global numeric_columns
     if request.method == 'POST':
         serializer = FormDataSerializer(data=request.data)
         if serializer.is_valid():
             data = serializer.validated_data
 
             playing_role = data['playing_role']
-            batting_style = data.get('batting_style')
-            bowling_style = data.get('bowling_style')
+            batting_style = data['batting_style']
+            bowling_style = data['bowling_style']
             age_min_value = data['age_min_value']
             age_max_value = data['age_max_value']
             selected_format = data['format']
             print(playing_role, batting_style, bowling_style, age_min_value, age_max_value, selected_format)
 
             query = Q()
+
             if playing_role:
                 query &= Q(playing_role=playing_role)
 
-            if playing_role == "Bowler":
-
-                numeric_columns = bowling_stats_order
-                if bowling_style:
-                    query &= Q(bowling_style=bowling_style)
-                if selected_format:
-                    query &= Q(playerbowling__format=selected_format)
-            elif playing_role == "Batsman":
-
-                numeric_columns = batting_stats_order
-                if batting_style:
-                    query &= Q(batting_style=batting_style)
-                if selected_format:
-                    query &= Q(playerbatting__format=selected_format)
+            if bowling_style:
+                query &= Q(bowling_style=bowling_style)
+            # if batting_style:
+            #     query &= Q(batting_style=batting_style)
 
             if age_min_value is not None and age_max_value is not None:
                 query &= Q(age__range=(age_min_value, age_max_value))
@@ -64,24 +40,22 @@ def rankPlayers(request):
             elif age_max_value is not None:
                 query &= Q(age__lte=age_max_value)
 
+            if selected_format:
+                query &= Q(playerbowling__format=selected_format)
+
             filtered_players = Player.objects.filter(query)
-            print(query)
-            player_list = []  # List to store dictionaries for each player
+
+            player_stats = []
+            print("lol1")
             for player in filtered_players:
-                player_dict = {
-                    'player_id': player.pk,
-                    'player_name': player.full_name,  # Assuming your Player model has a 'name' field
-                    'stats': []
-                }
-
-                player_stats = []
-
                 # Fetch relevant stats based on the playing role
-
+                print("lol")
                 if playing_role == 'Batsman':
+                    print("lol")
                     stats = PlayerBattingSerializer(
                         PlayerBatting.objects.filter(player=player, format=selected_format), many=True).data
                 elif playing_role == 'Bowler':
+                    print("lol")
                     stats = PlayerBowlingSerializer(
                         PlayerBowling.objects.filter(player=player, format=selected_format), many=True).data
                 elif playing_role == 'WicketKeeper':
@@ -89,67 +63,16 @@ def rankPlayers(request):
                         PlayerWicketKeeping.objects.filter(player=player, format=selected_format), many=True).data
                 else:
                     stats = []
-                for stat in stats:
-                    # Extract only the numerical values and append them to a list
-                    player_stats_values = [value for key, value in stat.items() if
-                                           key not in ['batting_id', 'format', 'player']]
 
-                    # Append the list of numerical values to the 'stats' key in the player_dict
-                    player_dict['stats'].append(player_stats_values)
+                player_serializer = PlayerSerializer(player).data
+                player_serializer.update({
+                    'stats': stats,
+                })
 
-                    # Append the player_dict to the player_list
-                player_list.append(player_dict)
+                player_stats.append(player_serializer)
+                print(player_stats)
 
-            # Get the absolute path of the current script
-            current_script_path = os.path.abspath(__file__)
-
-            # Get the content root directory (assuming this script is within the project)
-            content_root = os.path.dirname(os.path.dirname(os.path.dirname(current_script_path)))
-
-            if playing_role == 'Batsman' and selected_format == 'Test':
-                # Construct the path to the pickle file from the content root
-                relative_pickle_path = 'talentscout_backend/playeridentification/Pickle_models/trained_Batting_Test_model.pkl'
-                pickle_file_path = os.path.join(content_root, relative_pickle_path)
-
-            elif playing_role == 'Batsman' and selected_format == 'T20':
-                relative_pickle_path = 'talentscout_backend/playeridentification/Pickle_models/trained_Batting_T20_model.pkl'
-                pickle_file_path = os.path.join(content_root, relative_pickle_path)
-
-            elif playing_role == 'Batsman' and selected_format == 'Odi':
-                relative_pickle_path = 'talentscout_backend/playeridentification/Pickle_models/trained_Batting_ODI_model.pkl'
-                pickle_file_path = os.path.join(content_root, relative_pickle_path)
-
-            print(numeric_columns)
-            for player_info in player_list:
-                player_id = player_info['player_id']
-                player_name = player_info['player_name']
-                player_stats_list = player_info['stats']
-
-                print(f"Player ID: {player_id}, Player Name: {player_name}")
-
-                for stats_values in player_stats_list:
-                    print("Stats Values:", stats_values)
-
-                    # Calculate PPI using the pickle model
-                    with open(pickle_file_path, 'rb') as file:
-                        loaded_model = pickle.load(file)
-
-                    new_player_stats = pd.DataFrame([stats_values], columns=numeric_columns)
-                    predicted_ppi = loaded_model.predict(new_player_stats)
-
-                    # Update the stats_dict with the calculated PPI
-                    player_info['PPI'] = player_info.pop('stats')
-                    player_info['PPI'] = predicted_ppi
-
-            sorted_player_list = sorted(player_list, key=lambda x: x['PPI'], reverse=True)
-
-            for player_info in sorted_player_list:
-                player_id = player_info['player_id']
-                player_name = player_info['player_name']
-                PPI = player_info['PPI']
-
-                print(f"Player ID: {player_id}, Player Name: {player_name} , PPI: {PPI} ")
-            return Response(sorted_player_list, status=status.HTTP_200_OK)
+            return Response(player_stats, status=status.HTTP_200_OK)
 
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
