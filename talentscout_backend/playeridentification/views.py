af364d9a24c83d8cad846ba5bdef1629411f6ab1
M.D.C.D.Mahapatuna <109390299+ChamathDMahapatuna@users.noreply.github.com>
2024-03-12 21:22:51 +0530

    ML backend | using filters taking bowling stats to a list

--- talentscout_backend/playeridentification/views.py
+++ talentscout_backend/playeridentification/views.py
@@ -4,8 +4,7 @@ from rest_framework import status
 from .serializers import FormDataSerializer
 from crud_api.models import Player, PlayerBatting, PlayerBowling, PlayerWicketKeeping
 from django.db.models import Q
-from crud_api.serializers import PlayerSerializer, PlayerBattingSerializer, PlayerBowlingSerializer, \
-    PlayerWicketKeepingSerializer
+from crud_api.serializers import PlayerSerializer, PlayerBowlingSerializer
 
 
 @api_view(['POST'])
@@ -27,50 +26,40 @@ def rankPlayers(request):
 
             if playing_role:
                 query &= Q(playing_role=playing_role)
-
             if bowling_style:
                 query &= Q(bowling_style=bowling_style)
-            # if batting_style:
-            #     query &= Q(batting_style=batting_style)
-
+            print(query)
             if age_min_value is not None and age_max_value is not None:
                 query &= Q(age__range=(age_min_value, age_max_value))
             elif age_min_value is not None:
                 query &= Q(age__gte=age_min_value)
             elif age_max_value is not None:
                 query &= Q(age__lte=age_max_value)
-
             if selected_format:
                 query &= Q(playerbowling__format=selected_format)
 
             filtered_players = Player.objects.filter(query)
-
+            print(query)
             player_stats = []
-            print("lol1")
+
             for player in filtered_players:
-                # Fetch relevant stats based on the playing role
-                print("lol")
-                if playing_role == 'Batsman':
-                    print("lol")
-                    stats = PlayerBattingSerializer(
-                        PlayerBatting.objects.filter(player=player, format=selected_format), many=True).data
-                elif playing_role == 'Bowler':
-                    print("lol")
-                    stats = PlayerBowlingSerializer(
+
+                # Fetch only bowling stats for bowlers
+
+                if playing_role == 'Bowler':
+                    bowling_stats = PlayerBowlingSerializer(
                         PlayerBowling.objects.filter(player=player, format=selected_format), many=True).data
-                elif playing_role == 'WicketKeeper':
-                    stats = PlayerWicketKeepingSerializer(
-                        PlayerWicketKeeping.objects.filter(player=player, format=selected_format), many=True).data
                 else:
-                    stats = []
+                    bowling_stats = []  # Empty list for non-bowlers
+
+                # You can fetch batting stats and wicketkeeping stats if needed
 
                 player_serializer = PlayerSerializer(player).data
                 player_serializer.update({
-                    'stats': stats,
+                    'bowling_stats': bowling_stats,
                 })
 
                 player_stats.append(player_serializer)
-                print(player_stats)
 
             return Response(player_stats, status=status.HTTP_200_OK)
 
