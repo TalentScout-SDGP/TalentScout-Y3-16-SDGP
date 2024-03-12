bb58c6b6b8a0a6135478737af7e4c9f11e7e78a2
M.D.C.D.Mahapatuna <109390299+ChamathDMahapatuna@users.noreply.github.com>
2024-03-12 21:46:25 +0530

    ML backen | setting up test Batsman pickel filters

--- talentscout_backend/playeridentification/views.py
+++ talentscout_backend/playeridentification/views.py
@@ -111,13 +111,7 @@ def rankPlayers(request):
                 relative_pickle_path = 'talentscout_backend/playeridentification/Pickle_Model/trained_Batting_Test_model.pkl'
                 pickle_file_path = os.path.join(content_root, relative_pickle_path)
 
-            elif playing_role == 'Batsman' and selected_format == 'T20':
-                relative_pickle_path = 'talentscout_backend/playeridentification/Pickle_Model/trained_Batting_T20_model.pkl'
-                pickle_file_path = os.path.join(content_root, relative_pickle_path)
 
-            elif playing_role == 'Batsman' and selected_format == 'Odi':
-                relative_pickle_path = 'talentscout_backend/playeridentification/Pickle_Model/trained_Batting_ODI_model.pkl'
-                pickle_file_path = os.path.join(content_root, relative_pickle_path)
 
             print(numeric_columns)
             for player_info in player_list:
