1337762311224a508ccae15f0254b12cd304e4a9
SulanChandila <109819795+SulanChandila@users.noreply.github.com>
2024-03-13 19:09:37 +0530

    SDGP-375 UI testing configurations

---
+++ talentscout-frontend/src/pages/Tests/ComparePlayers.py
@@ -0,0 +1,59 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+class TestComparePlayersSearch(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("http://localhost:5173/compare_players")
+
+    def tearDown(self):
+        self.driver.quit()
+
+   #Test case -1
+    def test_search_without_selecting_players(self):
+        time.sleep(5)
+        # Locate the Compare button
+        compare_button = self.driver.find_element(By.XPATH, "//button[contains(text(),'Compare')]")
+
+        # Click the Compare button
+        compare_button.click()
+
+        # Wait for the toast error message to appear
+        WebDriverWait(self.driver, 5).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "Toastify__toast-body"))
+        )
+
+        # Get the toast error message text
+        toast_message = self.driver.find_element(By.CLASS_NAME, "Toastify__toast-body").text
+
+        # Assert that the toast error message matches the expected message
+        self.assertEqual(toast_message, "Please Select Two Players to Compare")
+        print('TEST 1 PASSED')
+
+    def test_tab_navigation(self):
+        # Check if tab navigation works correctly
+        time.sleep(5)
+        name1 = self.driver.find_element(By.XPATH,"/html/body/div/div/div[3]/div[1]/div/div/div/div[1]/input")
+        name1.click()
+        name1.send_keys("Dasun Dilshan")
+        time.sleep(2)
+        self.driver.find_element(By.XPATH,'/html/body/div/div/div[3]/div[1]/div/div/div/div[1]/div/div').click()
+        name2 = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div/div[2]/input")
+        name2.click()
+        name2.send_keys("Rahal Amarasinghe")
+        self.driver.find_element(By.XPATH,'/html/body/div/div/div[3]/div[1]/div/div/div/div[2]/div/div').click()
+        time.sleep(2)
+        compButton = self.driver.find_element(By.XPATH,'/html/body/div/div/div[3]/div[1]/div/div/div/button')
+        compButton.click()
+        time.sleep(2)
+        batting_tab = self.driver.find_element(By.XPATH,"//button[contains(text(), 'Batting')]")
+        batting_tab.click()
+        self.assertTrue(batting_tab.get_attribute("class").find("active") != -1)
+        print("TEST 2 PASSED")
+
+if __name__ == "__main__":
+    unittest.main()
