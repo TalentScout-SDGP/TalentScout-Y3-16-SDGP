badf568657cfaf0f74cb13a0e1adf5239494c9b9
SulanChandila <109819795+SulanChandila@users.noreply.github.com>
2024-03-21 19:54:46 +0530

    git commit -m "SDGP-381 ManagePlayers tests 5"

--- talentscout-frontend/src/pages/Tests/ManagePlayers.py
+++ talentscout-frontend/src/pages/Tests/ManagePlayers.py
@@ -164,6 +164,155 @@ class ManagePlayersTest(unittest.TestCase):
         assert cursor_property == "not-allowed", "Cursor is not set to not-allowed"
         print('TEST 5 PASSED')
 
+    # Test case - 6
+    def test_player_delete_restrictions(self):
+        """ Test Case to verify access restriction to edit or delete players who are not added to the system by the
+        logged in user"""
+        time.sleep(8)
+        log_pg = self.driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div[3]/a[1]")
+        log_pg.click()
+        time.sleep(5)
+        email_field = self.driver.find_element(By.XPATH, '//*[@id="login-email"]')
+        email_field.send_keys("qatestuser@gmail.com")
+
+        pas_field = self.driver.find_element(By.XPATH, '//*[@id="login-password"]')
+        pas_field.send_keys("talentscout@123")
+        time.sleep(5)
+
+        login_button = self.driver.find_element(By.XPATH,
+                                                "/html/body/div[1]/div/div[3]/div/div/div[2]/form/div/div[3]/button")
+        login_button.click()
+        time.sleep(10)
+
+        man_nav = self.driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div/div[2]/a[5]')
+        man_nav.click()
+        time.sleep(2)
+        search_inp = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div/input")
+        search_inp.click()
+        search_inp.send_keys('Kusal Janith')
+        time.sleep(3)
+        search = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div/button")
+        search.click()
+        time.sleep(10)
+        edit = self.driver.find_element(By.CSS_SELECTOR, "svg.text-xl:nth-child(2)")
+        cursor_property = edit.value_of_css_property("cursor")
+        assert cursor_property == "not-allowed", "Cursor is not set to not-allowed"
+        print('TEST 6 PASSED')
+
+    # Test case - 7
+    def test_player_info_edit_function(self):
+        """ Test Case to verify access to edit a player created by the logged in admin"""
+        time.sleep(8)
+        log_pg = self.driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div[3]/a[1]")
+        log_pg.click()
+        time.sleep(5)
+        email_field = self.driver.find_element(By.XPATH, '//*[@id="login-email"]')
+        email_field.send_keys("qatestuser@gmail.com")
+
+        pas_field = self.driver.find_element(By.XPATH, '//*[@id="login-password"]')
+        pas_field.send_keys("talentscout@123")
+        time.sleep(5)
+
+        login_button = self.driver.find_element(By.XPATH,
+                                                "/html/body/div[1]/div/div[3]/div/div/div[2]/form/div/div[3]/button")
+        login_button.click()
+        time.sleep(10)
+
+        man_nav = self.driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div/div[2]/a[5]')
+        man_nav.click()
+        time.sleep(2)
+        search_inp = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div/input")
+        search_inp.click()
+        search_inp.send_keys('Test Playerone')
+        time.sleep(3)
+        search = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div/button")
+        search.click()
+        time.sleep(10)
+        edit = self.driver.find_element(By.CSS_SELECTOR, "svg.text-xl:nth-child(1)")
+        edit.click()
+        time.sleep(2)
+        edit_name = self.driver.find_element(By.XPATH,
+                                             "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[1]/input")
+        edit_name.click()
+        edit_name.clear()
+        edited_name = "Test Player Edited"
+        edit_name.send_keys(edited_name)
+        time.sleep(2)
+        cont = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[4]/button")
+        cont.click()
+        time.sleep(2)
+        time.sleep(10)
+        sub = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[2]/div/div/div[3]/form/div[2]/button")
+        sub.click()
+        time.sleep(2)
+
+        WebDriverWait(self.driver, 5).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "Toastify__toast-body"))
+        )
+
+        # Get the toast error message text
+        toast_message = self.driver.find_element(By.CLASS_NAME, "Toastify__toast-body").text
+
+        # Assert that the toast error message matches the expected message
+        self.assertEqual(toast_message, "Player Updated Successfully!")
+        print("TEST 7 PASSED")
+
+    # Test case - 8
+    def test_player_stat_edit_function(self):
+        """ Test Case to verify access to edit a player created by the logged in admin"""
+        time.sleep(8)
+        log_pg = self.driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div[3]/a[1]")
+        log_pg.click()
+        time.sleep(5)
+        email_field = self.driver.find_element(By.XPATH, '//*[@id="login-email"]')
+        email_field.send_keys("qatestuser@gmail.com")
+
+        pas_field = self.driver.find_element(By.XPATH, '//*[@id="login-password"]')
+        pas_field.send_keys("talentscout@123")
+        time.sleep(5)
+
+        login_button = self.driver.find_element(By.XPATH,
+                                                "/html/body/div[1]/div/div[3]/div/div/div[2]/form/div/div[3]/button")
+        login_button.click()
+        time.sleep(10)
+
+        man_nav = self.driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div/div[2]/a[5]')
+        man_nav.click()
+        time.sleep(2)
+        search_inp = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div/input")
+        search_inp.click()
+        search_inp.send_keys('Test Playerone')
+        time.sleep(3)
+        search = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div/button")
+        search.click()
+        time.sleep(10)
+        edit = self.driver.find_element(By.CSS_SELECTOR, "svg.text-xl:nth-child(1)")
+        edit.click()
+        time.sleep(2)
+        edit_mat = self.driver.find_element(By.XPATH,
+                                            "/html/body/div/div/div[3]/div[2]/div/div/div[3]/form/div[1]/div/div[1]/input")
+        edit_mat.click()
+        edit_mat.clear()
+        edit_mat.send_keys(13)
+        cont = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[4]/button")
+        cont.click()
+        time.sleep(2)
+        time.sleep(10)
+        sub = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[2]/div/div/div[3]/form/div[2]/button")
+        sub.click()
+        time.sleep(2)
+
+        WebDriverWait(self.driver, 5).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "Toastify__toast-body"))
+        )
+
+        # Get the toast error message text
+        toast_message = self.driver.find_element(By.CLASS_NAME, "Toastify__toast-body").text
+
+        # Assert that the toast error message matches the expected message
+        self.assertEqual(toast_message, "Player Updated Successfully!")
+        print("TEST 8 PASSED")
+
 
 
 
