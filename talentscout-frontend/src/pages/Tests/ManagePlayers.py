b05cd78e475ce990c4c4a00d31c8e1dff3031a5a
SulanChandila <109819795+SulanChandila@users.noreply.github.com>
2024-03-21 19:53:45 +0530

    git commit -m "SDGP-381 ManagePlayers tests 2"

--- talentscout-frontend/src/pages/Tests/ManagePlayers.py
+++ talentscout-frontend/src/pages/Tests/ManagePlayers.py
@@ -31,6 +31,69 @@ class ManagePlayersTest(unittest.TestCase):
         assert restriction_notification.is_displayed()
         print('TEST 1 PASSED')
 
+    # Test case - 2
+    def test_page_accessibility_to_admin_users(self):
+        """ Test Case to verify Accessibility to the Manage Players page for admin users."""
+        time.sleep(8)
+        log_pg = self.driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div[3]/a[1]")
+        log_pg.click()
+        time.sleep(5)
+        email_field = self.driver.find_element(By.XPATH, '//*[@id="login-email"]')
+        email_field.send_keys("qatestuser@gmail.com")
+
+        pas_field = self.driver.find_element(By.XPATH, '//*[@id="login-password"]')
+        pas_field.send_keys("talentscout@123")
+        time.sleep(5)
+
+        login_button = self.driver.find_element(By.XPATH,
+                                                "/html/body/div[1]/div/div[3]/div/div/div[2]/form/div/div[3]/button")
+        login_button.click()
+        time.sleep(10)
+
+        man_nav = self.driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div/div[2]/a[5]')
+        man_nav.click()
+        time.sleep(2)
+
+        cont = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div")
+        assert cont.is_displayed()
+        print('TEST 2 PASSED')
+
+    # Test case - 3
+    def test_search_with_no_inputs(self):
+        """ Test Case to verify error handling when clicking search without selecting a player"""
+        time.sleep(8)
+        log_pg = self.driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div[3]/a[1]")
+        log_pg.click()
+        time.sleep(5)
+        email_field = self.driver.find_element(By.XPATH, '//*[@id="login-email"]')
+        email_field.send_keys("qatestuser@gmail.com")
+
+        pas_field = self.driver.find_element(By.XPATH, '//*[@id="login-password"]')
+        pas_field.send_keys("talentscout@123")
+        time.sleep(5)
+
+        login_button = self.driver.find_element(By.XPATH,
+                                                "/html/body/div[1]/div/div[3]/div/div/div[2]/form/div/div[3]/button")
+        login_button.click()
+        time.sleep(10)
+
+        man_nav = self.driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div/div[2]/a[5]')
+        man_nav.click()
+        time.sleep(2)
+        search = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div/button")
+        search.click()
+        time.sleep(5)
+        WebDriverWait(self.driver, 5).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "Toastify__toast-body"))
+        )
+
+        # Get the toast error message text
+        toast_message = self.driver.find_element(By.CLASS_NAME, "Toastify__toast-body").text
+
+        # Assert that the toast error message matches the expected message
+        self.assertEqual(toast_message, "Please enter a player name to search.")
+        print('TEST 3 PASSED')
+
 
 
 
