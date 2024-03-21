63c614eae6f8ecd5f01b41b69a9353d63803b35b
SulanChandila <109819795+SulanChandila@users.noreply.github.com>
2024-03-21 19:47:31 +0530

    git commit -m "SDGP-379 Compare Players tests 2"

--- talentscout-frontend/src/pages/Tests/ComparePlayers.py
+++ talentscout-frontend/src/pages/Tests/ComparePlayers.py
@@ -35,6 +35,64 @@ class TestComparePlayersSearch(unittest.TestCase):
         self.assertEqual(toast_message, "Please Select Two Players to Compare")
         print('TEST 1 PASSED')
 
+    # Test case - 2
+    def test_searching_two_players(self):
+        """ Test Case to verify that Searched players are getting loaded for Comparison"""
+        time.sleep(5)
+        search_input_1 = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div/div[1]/input")
+        expected_result_1 = search_input_1.send_keys("Rahal Amarasinghe")
+        time.sleep(2)
+        select_dropdown = self.driver.find_element(By.XPATH,
+                                                   '/html/body/div/div/div[3]/div[1]/div/div/div/div[1]/div/div')
+        select_dropdown.click()
+        time.sleep(2)
+        search_input_2 = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div/div[2]/input")
+        expected_result_2 = search_input_2.send_keys("B S N Kasun")
+        time.sleep(2)
+        select_dropdown = self.driver.find_element(By.XPATH,
+                                                   '/html/body/div/div/div[3]/div[1]/div/div/div/div[2]/div/div')
+        select_dropdown.click()
+        compare_button = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div/button")
+
+        compare_button.click()
+        time.sleep(5)
+        # Wait for dropdown to appear
+        name = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[2]/div[1]/div/div/div[1]/p")
+        actual_result_1 = name.text
+        time.sleep(2)
+        self.assertEqual(actual_result_1, "Rahal Amarasinghe",
+                         f"Page title is incorrect: Expected '{"Rahal Amarasinghe"}' but found '{actual_result_1}'")
+        name_2 = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[2]/div[1]/div/div/div[3]/p")
+        actual_result_2 = name_2.text
+        time.sleep(2)
+        self.assertEqual(actual_result_2, "B S N Kasun",
+                         f"Page title is incorrect: Expected '{"B S N Kasun"}' but found '{actual_result_2}'")
+        print("TEST 2 PASSED SUCCESSFULLY")
+        self.driver.refresh()
+        time.sleep(5)
+
+    # Test case - 3
+    def test_tab_navigation(self):
+        """ Test Case to verify navigation across different statistics and formats in Comparison"""
+        time.sleep(5)
+        name1 = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div/div[1]/input")
+        name1.click()
+        name1.send_keys("Dasun Dilshan")
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, '/html/body/div/div/div[3]/div[1]/div/div/div/div[1]/div/div').click()
+        name2 = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div/div[2]/input")
+        name2.click()
+        name2.send_keys("Rahal Amarasinghe")
+        self.driver.find_element(By.XPATH, '/html/body/div/div/div[3]/div[1]/div/div/div/div[2]/div/div').click()
+        time.sleep(2)
+        compButton = self.driver.find_element(By.XPATH, '/html/body/div/div/div[3]/div[1]/div/div/div/button')
+        compButton.click()
+        time.sleep(2)
+        batting_tab = self.driver.find_element(By.XPATH, "//button[contains(text(), 'Bowling')]")
+        batting_tab.click()
+        self.assertTrue(batting_tab.get_attribute("class").find("active") != -1)
+        print("TEST 3 PASSED")
+
 
 
 
