96fc9976fb44104a6081c2daf61fa8e7c2156423
SulanChandila <109819795+SulanChandila@users.noreply.github.com>
2024-03-21 19:57:17 +0530

    git commit -m "SDGP-384 SignUp tests 2"

--- talentscout-frontend/src/pages/Tests/Signup.py
+++ talentscout-frontend/src/pages/Tests/Signup.py
@@ -27,6 +27,34 @@ class SignUpTest(unittest.TestCase):
         self.assertTrue("date")
         print("TEST 1 PASSED")
 
+    # Test case - 2
+    def test_password_length_restriction(self):
+        """ Test Case to verify password length is greater than 7"""
+        time.sleep(5)
+        pas = self.driver.find_element(By.ID, "sign-up-password")
+        pas.click()
+        pas.send_keys("111111")
+        time.sleep(2)
+        err = self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[1]/form/div[7]/p")
+        self.assertEqual(
+            "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.",
+            err.text)
+        print("TEST 2 PASSED")
+
+    # Test case - 3
+    def test_password_character_restriction(self):
+        """ Test Case to verify password character restrictions"""
+        time.sleep(5)
+        pas = self.driver.find_element(By.ID, "sign-up-password")
+        pas.click()
+        pas.send_keys("12345678")
+        time.sleep(2)
+        err = self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div/div[1]/div[1]/form/div[7]/p")
+        self.assertEqual(
+            "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.",
+            err.text)
+        print("TEST 3 PASSED")
+
 
 
 
