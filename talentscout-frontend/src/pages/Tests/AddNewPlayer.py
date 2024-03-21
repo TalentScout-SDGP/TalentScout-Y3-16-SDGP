4a20c50e88675c0c97d5371af362f0289bd64969
SulanChandila <109819795+SulanChandila@users.noreply.github.com>
2024-03-21 19:44:24 +0530

    git commit -m "SDGP-382 Add New Players tests 2"

--- talentscout-frontend/src/pages/Tests/AddNewPlayer.py
+++ talentscout-frontend/src/pages/Tests/AddNewPlayer.py
@@ -50,6 +50,147 @@ class ManagePlayersTest(unittest.TestCase):
         assert add_section.is_displayed(), "Page not rendered successfully"
         print('TEST 1 PASSED')
 
+    # Test case - 2
+    def test_continuing_player_profileInfo_with_empty_required_fields(self):
+        """ Test Case to verify error handling when submiting in Player profile section by keeping at least one
+        required field empty"""
+        time.sleep(8)
+        log_pg = self.driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div[3]/a[1]")
+        log_pg.click()
+        time.sleep(5)
+        email_field = self.driver.find_element(By.XPATH, '//*[@id="login-email"]')
+        email_field.send_keys("qatestuser@gmail.com")
+
+        pas_field = self.driver.find_element(By.XPATH, '//*[@id="login-password"]')
+        pas_field.send_keys("talentscout@123")
+        time.sleep(5)
+
+        login_button = self.driver.find_element(By.XPATH,
+                                                "/html/body/div[1]/div/div[3]/div/div/div[2]/form/div/div[3]/button")
+        login_button.click()
+        time.sleep(10)
+
+        man_nav = self.driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div/div[2]/a[5]')
+        man_nav.click()
+        time.sleep(2)
+
+        add_new_p = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div/a")
+        add_new_p.click()
+        time.sleep(3)
+        fname = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[1]/input")
+        fname.click()
+        fname.send_keys("Test Player 1")
+        time.sleep(1)
+        alsKnown = self.driver.find_element(By.XPATH,
+                                            "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[2]/input")
+        alsKnown.click()
+        alsKnown.send_keys("T P 1")
+        time.sleep(1)
+        bdate = self.driver.find_element(By.XPATH,
+                                         "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[3]/div[1]/input")
+        bdate.click()
+        bdate.send_keys("2222002")
+        time.sleep(1)
+        bstyle = self.driver.find_element(By.XPATH,
+                                          "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[3]/div[3]/select")
+        bstyle.click()
+        batOpt = self.driver.find_element(By.XPATH,
+                                          "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[3]/div[3]/select/option[2]")
+        batOpt.click()
+        time.sleep(2)
+        bowlstyle = self.driver.find_element(By.XPATH,
+                                             "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[3]/div[4]/select")
+        bowlstyle.click()
+        bowlOpt = self.driver.find_element(By.XPATH,
+                                           "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[3]/div[4]/select/option[4]")
+        bowlOpt.click()
+        time.sleep(2)
+        # not selecting the playing role and clicking continue
+        contBut = self.driver.find_element(By.XPATH,
+                                           "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[4]/button")
+        contBut.click()
+        time.sleep(1)
+        WebDriverWait(self.driver, 5).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "Toastify__toast-body"))
+        )
+        toast_message = self.driver.find_element(By.CLASS_NAME, "Toastify__toast-body").text
+        self.assertEqual(toast_message, "Please fill in all the required fields (*).")
+        print('TEST 2 PASSED')
+
+    # Test case -3
+    def test_player_info_addition(self):
+        """ Test Case to verify error handling when submitting in Player profile section by keeping at least one
+        required field empty"""
+        time.sleep(8)
+        log_pg = self.driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div[3]/a[1]")
+        log_pg.click()
+        time.sleep(5)
+        email_field = self.driver.find_element(By.XPATH, '//*[@id="login-email"]')
+        email_field.send_keys("qatestuser@gmail.com")
+
+        pas_field = self.driver.find_element(By.XPATH, '//*[@id="login-password"]')
+        pas_field.send_keys("talentscout@123")
+        time.sleep(5)
+
+        login_button = self.driver.find_element(By.XPATH,
+                                                "/html/body/div[1]/div/div[3]/div/div/div[2]/form/div/div[3]/button")
+        login_button.click()
+        time.sleep(10)
+
+        man_nav = self.driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div/div[2]/a[5]')
+        man_nav.click()
+        time.sleep(2)
+
+        add_new_p = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div/a")
+        add_new_p.click()
+        time.sleep(3)
+        fname = self.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[1]/input")
+        fname.click()
+        fname.send_keys("Test Player 1")
+        time.sleep(1)
+        alsKnown = self.driver.find_element(By.XPATH,
+                                            "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[2]/input")
+        alsKnown.click()
+        alsKnown.send_keys("T P 1")
+        time.sleep(1)
+        bdate = self.driver.find_element(By.XPATH,
+                                         "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[3]/div[1]/input")
+        bdate.click()
+        bdate.send_keys("2222002")
+        time.sleep(1)
+        bstyle = self.driver.find_element(By.XPATH,
+                                          "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[3]/div[3]/select")
+        bstyle.click()
+        batOpt = self.driver.find_element(By.XPATH,
+                                          "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[3]/div[3]/select/option[2]")
+        batOpt.click()
+        time.sleep(2)
+        bowlstyle = self.driver.find_element(By.XPATH,
+                                             "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[3]/div[4]/select")
+        bowlstyle.click()
+        bowlOpt = self.driver.find_element(By.XPATH,
+                                           "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[3]/div[4]/select/option[4]")
+        bowlOpt.click()
+        time.sleep(2)
+        plyRole = self.driver.find_element(By.XPATH,
+                                           "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[3]/div[2]/select")
+        plyRole.click()
+        roleOpt = self.driver.find_element(By.XPATH,
+                                           "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[3]/div[2]/select/option[4]")
+        roleOpt.click()
+        time.sleep(2)
+        # not selecting the playing role and clicking continue
+        contBut = self.driver.find_element(By.XPATH,
+                                           "/html/body/div/div/div[3]/div[1]/div/div/div[2]/form/div[4]/button")
+        contBut.click()
+        time.sleep(1)
+        WebDriverWait(self.driver, 5).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "Toastify__toast-body"))
+        )
+        toast_message = self.driver.find_element(By.CLASS_NAME, "Toastify__toast-body").text
+        self.assertEqual(toast_message, "Player Info added, Proceed to add Player Stats.")
+        print('TEST 3 PASSED')
+
 
 
 
