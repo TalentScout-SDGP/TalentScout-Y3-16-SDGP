34e5ae1a7f12e218741048e0c755ba9a087b70e6
M.D.C.D.Mahapatuna <109390299+ChamathDMahapatuna@users.noreply.github.com>
2024-03-24 18:46:33 +0530

    SDGP-386 ML backed Testing | Testcase 4 T20_Batting

--- talentscout_backend/playeridentification/tests.py
+++ talentscout_backend/playeridentification/tests.py
@@ -165,6 +165,23 @@ class TestBackendFunctionality(unittest.TestCase):
             relative_pickle_path = 'talentscout_backend/playeridentification/Pickle_models/trained_Batting_T20_model.pkl'
             pickle_file_path = os.path.join(content_root, relative_pickle_path)
 
+            with open(pickle_file_path,
+                      'rb') as model_file:
+                loaded_model = pickle.load(model_file)
+            new_player_stats = pd.DataFrame([Bowling_stats],
+                                            columns=['Matches', 'Runs', 'Innings', 'NO', 'HS', 'Avg', 'BF', 'SR',
+                                                     '100s', '50s', '4s', '6s'])
+            predicted_ppi = loaded_model.predict(new_player_stats)
+            expected_ppi = round(predicted_ppi[0], 2)
+
+            # Act
+            actual_ppi = calculate_ppi(format_input, playing_role, Bowling_stats)
+            actual_ppi = round(actual_ppi, 2)
+
+            # Assert
+            self.assertEqual(actual_ppi, expected_ppi)
+
+
 
 
 
