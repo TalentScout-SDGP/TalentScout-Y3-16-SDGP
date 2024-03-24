1ad7d6f2082fa9f8cb0e3ad7445b42f4fc6c8117
M.D.C.D.Mahapatuna <109390299+ChamathDMahapatuna@users.noreply.github.com>
2024-03-24 18:50:41 +0530

    SDGP-386 ML backed Testing | Testcase 8 T20_WK

--- talentscout_backend/playeridentification/tests.py
+++ talentscout_backend/playeridentification/tests.py
@@ -265,6 +265,32 @@ class TestBackendFunctionality(unittest.TestCase):
             # Assert
             self.assertEqual(actual_ppi, expected_ppi)
 
+        def test_calculate_ppi_T20_WK(self):
+            # Arrange
+            format_input = "t20"
+            playing_role = "wicket keeping"
+            Bowling_stats = [4, 4, 0, 0]
+            current_script_path = os.path.abspath(__file__)
+            # Get the content root directory (assuming this script is within the project)
+            content_root = os.path.dirname(os.path.dirname(os.path.dirname(current_script_path)))
+
+            relative_pickle_path = 'talentscout_backend/playeridentification/Pickle_models/trained_WK_T20_model.pkl'
+            pickle_file_path = os.path.join(content_root, relative_pickle_path)
+
+            with open(pickle_file_path,
+                      'rb') as model_file:
+                loaded_model = pickle.load(model_file)
+            new_player_stats = pd.DataFrame([Bowling_stats],
+                                            columns=['Matches', 'Innings', 'Ct', 'St'])
+            predicted_ppi = loaded_model.predict(new_player_stats)
+            expected_ppi = round(predicted_ppi[0], 2)
+
+            # Act
+            actual_ppi = calculate_ppi(format_input, playing_role, Bowling_stats)
+            actual_ppi = round(actual_ppi, 2)
+
+            # Assert
+            self.assertEqual(actual_ppi, expected_ppi)
 
 
 
