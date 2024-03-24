e12f4a3b51e111bc07034d70deae7094abe8ba6b
M.D.C.D.Mahapatuna <109390299+ChamathDMahapatuna@users.noreply.github.com>
2024-03-24 18:28:48 +0530

    SDGP-386 ML backed Testing | Define weightage types for batting, bowling, and wicket keeping

--- talentscout_backend/playeridentification/tests.py
+++ talentscout_backend/playeridentification/tests.py
@@ -18,3 +18,17 @@ bowling_stats_order = ['Matches', 'Wickets', 'Innings', 'Overs', 'Runs', 'BBI', 
 batting_stats_order = ['Matches', 'Runs', 'Innings', 'NO', 'HS', 'Avg', 'BF', 'SR', '100s', '50s', '4s', '6s']
 stats = []
 
+
+def calculate_ppi(format_input, playing_role, stats):
+
+    # Define weightage types for batting, bowling, and wicket keeping
+    batting_weights = {
+        "test": {"Matches": 0.08, "Runs": 0.20, "Innings": 0.08, "NO": 0.06, "HS": 0.06, "Avg": 0.22, "BF": 0.08,
+                 "SR": 0.07, "100s": 0.08, "50s": 0.04, "4s": 0.04, "6s": 0.04},
+        "odi": {"Matches": 0.07, "Runs": 0.24, "Innings": 0.07, "NO": 0.06, "HS": 0.06, "Avg": 0.18, "BF": 0.08,
+                "SR": 0.10,
+                "100s": 0.10, "50s": 0.06, "4s": 0.05, "6s": 0.05},
+        "t20": {"Matches": 0.06, "Runs": 0.21, "Innings": 0.06, "NO": 0.06, "HS": 0.06, "Avg": 0.15, "BF": 0.07,
+                "SR": 0.13,
+                "100s": 0.07, "50s": 0.04, "4s": 0.04, "6s": 0.05}
+    }
